badd7a89108cc45cb96e9d98b5c119a326fe0d18
GojiMiin <got.j@windowslive.com>
2023-03-26 23:39:21 +0700

    refactor: refactor process

--- challenge_1/main.go
+++ challenge_1/main.go
@@ -4,42 +4,40 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 )
 
 func main() {
-	data := readFile()
-	fmt.Println("data len : ", len(data))
+	data, readErr := readFile()
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
 
 	// process data
 	dataLoc := 0
 	summary := data[0][0]
-	fmt.Println("start summary : ", summary)
+	fmt.Println("start process")
 	data = data[1:]
 	for i := range data {
-		// fmt.Println(i, v)
-		fmt.Println(data[i])
-		if data[i][dataLoc] > data[i][dataLoc+1] {
-			summary += data[i][dataLoc]
-		} else {
-			summary += data[i][dataLoc+1]
-			dataLoc = dataLoc + 1
+		if data[i][dataLoc] < data[i][dataLoc+1] {
+			dataLoc += 1
 		}
-		fmt.Println(data[i][dataLoc])
+		summary += data[i][dataLoc]
 	}
 	fmt.Println("result : ", summary)
 }
 
-func readFile() [][]int {
+func readFile() ([][]int, error) {
 	var arr [][]int
 	jsonFile, openErr := ioutil.ReadFile("files/hard.json")
 	if openErr != nil {
-		fmt.Println(openErr)
+		return nil, openErr
 	}
 
 	unmarshalErr := json.Unmarshal(jsonFile, &arr)
 	if unmarshalErr != nil {
-		fmt.Println(unmarshalErr)
+		return nil, unmarshalErr
 	}
 
-	return arr
+	return arr, nil
 }
