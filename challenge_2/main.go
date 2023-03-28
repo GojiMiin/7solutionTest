05dc7fdbd696815f2e05f82231aaa82d7703f2a5
GojiMiin <got.j@windowslive.com>
2023-03-28 21:18:40 +0700

    refactor: edit challenge 2 logic

--- challenge_2/main.go
+++ challenge_2/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -11,40 +13,63 @@ func main() {
 
 	// input
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter encoding: ")
+	fmt.Print("input = ")
 	input, _ := reader.ReadString('\n')
 	input = strings.ToUpper(input[:len(input)-2])
-	fmt.Println("input : ", len(input))
 
-	repeatLetter, repeatTimes, repeatLoc := countRepeatLetter(input)
-	fmt.Println("letter : ", repeatLetter)
-	fmt.Println("mostRepeat : ", repeatTimes)
-	fmt.Println("loc : ", repeatLoc)
-}
+	decodeArr, initErr := initDecodeArr(string(input[0]))
+	numberToAdd := 0
+	result := ""
 
-func countRepeatLetter(input string) (string, int, int) {
-	currentRepeat := 1
-	repeatLetter := "L"
-	startRepeatLoc := 0
-	mostRepeat := 1
-	mostRepeatLetter := ""
-	startMostRepeat := 0
-
-	for i, v := range input {
-		letter := string(v)
-		if letter == repeatLetter {
-			currentRepeat += 1
-		} else {
-			if currentRepeat > 1 && currentRepeat > mostRepeat {
-				mostRepeat = currentRepeat
-				mostRepeatLetter = repeatLetter
-				startMostRepeat = startRepeatLoc
-			}
-			currentRepeat = 1
-			repeatLetter = letter
-			startRepeatLoc = i
-		}
+	if initErr != nil {
+		log.Fatal(initErr)
+		os.Exit(1)
 	}
 
-	return mostRepeatLetter, mostRepeat, startMostRepeat
+	for i, v := range input[1:] {
+		decodeArr = compareEncode(string(v), i, decodeArr, &numberToAdd)
+	}
+
+	for i := 0; i < len(decodeArr); i++ {
+		decodeArr[i] = decodeArr[i] + numberToAdd
+		result += strconv.Itoa(decodeArr[i])
+	}
+
+	fmt.Println("output = ", result)
+
+}
+
+func initDecodeArr(firstLetter string) ([]int, error) {
+	switch firstLetter {
+	case "L":
+		return []int{1, 0}, nil
+	case "R":
+		return []int{0, 1}, nil
+	case "=":
+		return []int{0, 0}, nil
+	default:
+		return []int{}, fmt.Errorf("wrong letter set")
+	}
+}
+
+func compareEncode(curLetter string, curLoc int, decoded []int, numberToAdd *int) []int {
+	curDecoded := decoded[curLoc+1]
+	switch curLetter {
+	case "L":
+		nextDecoded := curDecoded - 1
+		if nextDecoded < 0 && nextDecoded < *numberToAdd {
+			*numberToAdd = -nextDecoded
+		}
+		newDecoded := append(decoded, nextDecoded)
+		return newDecoded
+	case "R":
+		nextDecoded := curDecoded + 1
+		newDecoded := append(decoded, nextDecoded)
+		return newDecoded
+	case "=":
+		newDecoded := append(decoded, curDecoded)
+		return newDecoded
+	default:
+		return []int{}
+	}
 }
